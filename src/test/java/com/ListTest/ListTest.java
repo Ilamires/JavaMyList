2e6628725867c87c8032db7b72d4982688c295ba
Ilamires <v.shamraev1@yandex.ru>
2025-03-21 16:00:47 +0300

    Add test to GetIndexByValue and RemoveByValue methods from MyList

--- src/test/java/com/ListTest/ListTest.java
+++ src/test/java/com/ListTest/ListTest.java
@@ -16,4 +16,31 @@ public class ListTest {
                 myList.GetByIndex(1) == 43 &&
                 myList.GetByIndex(2) == 4673);
     }
+
+    @Test
+    public void MyListGetIndexByValueTest() {
+        MyList<Integer> myList = new MyList<Integer>();
+        myList.PushBack(1);
+        myList.PushBack(43);
+        myList.PushBack(4673);
+        Assert.assertTrue(myList.GetIndexByValue(80) == -1 &&
+                myList.GetIndexByValue(1) == 0 &&
+                myList.GetIndexByValue(43) == 1 &&
+                myList.GetIndexByValue(4673) == 2);
+    }
+
+    @Test
+    public void MyListRemoveByValueTest() {
+        MyList<Integer> myList = new MyList<Integer>();
+        myList.PushBack(43);
+        myList.PushBack(43);
+        myList.PushBack(4673);
+        myList.PushBack(4884);
+        myList.RemoveByValue(4673);
+        myList.RemoveByValue(43);
+        Assert.assertTrue(myList.GetIndexByValue(43) == 0 &&
+                myList.GetIndexByValue(4673) == -1 &&
+                myList.GetIndexByValue(4884) == 1);
+    }
+
 }
