a2889e2e45abff688f133c49f339fd4e4eb58c10
Ilamires <v.shamraev1@yandex.ru>
2025-03-21 13:21:03 +0300

    Add template and Node to MyList

--- src/main/java/com/MyList/MyList.java
+++ src/main/java/com/MyList/MyList.java
@@ -1,4 +1,19 @@
 package com.MyList;
 
-public class MyList {
+public class MyList<T> {
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        Node(T value) {
+            this(value, null);
+        }
+    }
+
+    Node<T> head;
 }
